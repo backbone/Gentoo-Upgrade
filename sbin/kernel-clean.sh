072efc726fbc72911c386c30e606c1df011a4ba1
Kolan Sh <mecareful@gmail.com>
2013-04-01 15:07:13 +0400

    kernel-clean: autoremount ro/rw

--- sbin/kernel-clean.sh
+++ sbin/kernel-clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ -f /etc/gentoo-upgrade.conf ] && source /etc/gentoo-upgrade.conf
+
 NICE_CMD="nice -n 19 ionice -c2"
 
 REVISION=`kernel-config list | grep \*$ | cut -d" " -f6 | cut -d- -f2-8`
@@ -7,6 +9,15 @@ REVISION=`kernel-config list | grep \*$ | cut -d" " -f6 | cut -d- -f2-8`
 
 SOURCES=linux-$REVISION
 [ "" == "$SOURCES" ] && echo "No appropriate kernel sources found ;-(" && exit -1
+
+# remounting file systems ro->rw
+for fs in $RW_REMOUNT; do
+	if [[ "$fs" =~ ^/+usr/*$ || "$fs" =~ ^/+boot/*$ ]]; then
+		echo "remounting $fs -> rw"
+		mount -o remount,rw $fs
+		[ 0 -ne $? ] && echo "mount -o remount,rw $fs failed ;-( =======" && exit -1
+	fi
+done
 
 # rm old modules
 cd /lib/modules && $NICE_CMD rm -rf `ls | grep -v "^$REVISION$"`
@@ -18,6 +29,15 @@ mount -o remount,ro /boot
 
 # rm old sources
 cd /usr/src && $NICE_CMD rm -rf `find -maxdepth 1 -name "linux-*" -type d | grep -v "$SOURCES$"`
+
+# remounting file systems rw->ro
+for fs in $RO_REMOUNT; do
+	if [[ "$fs" =~ ^/+usr/*$ || "$fs" =~ ^/+boot/*$ ]]; then
+		echo "remounting $fs -> ro"
+		mount -o remount,ro $fs
+		[ 0 -ne $? ] && echo "mount -o remount,ro $fs failed ;-( =======" && exit -1
+	fi
+done
 
 exit 0
 
