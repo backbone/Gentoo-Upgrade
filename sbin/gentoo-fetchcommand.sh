8df4b7e9b37ebe0ae63b10f7a46bbd8ff9139c00
Kolan Sh <mecareful@gmail.com>
2013-09-12 01:09:45 +0400

    gentoo-fetchcommand script updated

--- sbin/gentoo-fetchcommand.sh
+++ sbin/gentoo-fetchcommand.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-DEST_FILE=`echo $@ | sed 's~.* ~~g'`
-/usr/bin/getdelta.sh "$@"
-if [ ! -f "$DEST_FILE" ]; then
-        echo "======= FULL DOWNLOAD... ======"
-        echo "======= PARAMS: $@ ======"
-        /usr/bin/wget -t1 --passive-ftp $@
+echo "======= PARAMS: $@ ======"
+URL=`echo $@ | awk '{print $1}'`
+DEST_FILE=`echo $@ | awk '{print $3}'`
+
+/usr/bin/getdelta.sh $URL
+
+if [ -f "$DEST_FILE" ]; then
+	exit 0
+else
+	echo "======= FULL DOWNLOAD... ======"
+	echo "======= PARAMS: $@ ======"
+	URL_FNAME=${URL##*/}
+	DEST_FILE_DIR=${DEST_FILE%/*}
+	if [[ ! -z "$URL_FNAME" && ! -z "${DEST_FILE##*/}"
+	   && ! -z "$DEST_FILE_DIR" && -f ${DEST_FILE%/*}/$URL_FNAME ]]; then
+		echo --- MOVING $URL_FNAME TO ${DEST_FILE##*/} ---
+		mv -f $DEST_FILE_DIR/$URL_FNAME $DEST_FILE
+		exit $?
+	else
+		/usr/bin/wget -t1 --passive-ftp $@
+		exit $?
+	fi
 fi
 
-exit 0
+exit -1
 
